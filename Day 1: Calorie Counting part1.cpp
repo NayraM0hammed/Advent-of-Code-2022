0446133cde7e92a95b75776e5de95a7e5a37a578
Nayra Mohammed <97265976+NayraM0hammed@users.noreply.github.com>
2022-12-01 16:45:35 +0200

    Create Day 1: Calorie Counting part1.cpp

---
+++ Day 1: Calorie Counting part1.cpp
@@ -0,0 +1,52 @@
+//"Perhaps..,we 're asking the wrong questions .." -Agent Brown
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <iomanip>
+#include <unordered_map>
+#define sz(s) (int(s.size()))
+#define MemS(s, x) memset(s, x, sizeof(s))
+#define PI acos(-1)
+typedef long long ll;
+using namespace std;
+
+void SADIEM()
+{
+    freopen("input.txt", "r", stdin);
+    //    freopen("output.txt", "w", stdout);
+
+    std::ios_base::sync_with_stdio(NULL);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+/*
+
+ “ I’d like to let you in on a very important secret I learned when I was about your age, boy.
+ You see, power, real power doesn’t come to those who were born strongest or fastest or smartest.
+ No. It comes to those who will do anything to achieve it.”
+
+*/
+void Catch_Kadr_fe_ALOLo()
+{
+    string s;
+    ll mx = 0, sum = 0;
+
+    while (getline(cin, s))
+        (s == "") ? (mx = max(mx, sum), sum = 0) : sum += stoi(s);
+
+    mx = max(mx, sum);
+    cout << mx;
+}
+
+int main()
+{
+    SADIEM();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Catch_Kadr_fe_ALOLo();
+        cout << (t ? "\n" : "");
+    }
+}
