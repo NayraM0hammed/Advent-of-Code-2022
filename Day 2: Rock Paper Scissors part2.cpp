6ce32b48a454245eee7f21074bf1d37d7b58694e
Nayra Mohammed <97265976+NayraM0hammed@users.noreply.github.com>
2022-12-02 11:59:35 +0200

    Create Day 2: Rock Paper Scissors part2.cpp

---
+++ Day 2: Rock Paper Scissors part2.cpp
@@ -0,0 +1,58 @@
+//"Perhaps..,we 're asking the wrong questions .." -Agent Brown
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <iomanip>
+#include <unordered_map>
+#define sz(s) (int(s.size()))
+#define MemS(s, x) memset(s, x, sizeof(s))
+#define PI acos(-1)
+typedef long long ll;
+using namespace std;
+
+void SADIEM()
+{
+    freopen("input.txt", "r", stdin);
+    //    freopen("output.txt", "w", stdout);
+
+    std::ios_base::sync_with_stdio(NULL);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+/*
+
+ “ I’d like to let you in on a very important secret I learned when I was about your age, boy.
+ You see, power, real power doesn’t come to those who were born strongest or fastest or smartest.
+ No. It comes to those who will do anything to achieve it.”
+
+*/
+void Catch_Kadr_fe_ALOLo()
+{
+    char p1, p2;
+    int sc = 0;
+  
+    while (cin >> p1 >> p2)
+    {
+        if (p2 == 'X')
+            sc += (p1 == 'A' ? 3 : p1 == 'B' ? 1 : 2);
+        else if (p2 == 'Y')
+            sc += (p1 - 'A' + 1) + 3;
+        else
+            sc += (p1 == 'A' ? 2 : p1 == 'B' ? 3 : 1) + 6;
+    }
+  
+    cout << sc;
+}
+
+int main()
+{
+    SADIEM();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Catch_Kadr_fe_ALOLo();
+        cout << (t ? "\n" : "");
+    }
+}
