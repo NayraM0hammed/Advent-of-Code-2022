0bb6fe9ee919b2c1703cc206eed1e120960858f6
Nayra Mohammed <97265976+NayraM0hammed@users.noreply.github.com>
2022-12-04 09:53:24 +0200

    Create Part2 .cpp

---
+++ Day 4: Camp Cleanup/Part2 .cpp
@@ -0,0 +1,60 @@
+//"Perhaps..,we 're asking the wrong questions .." -Agent Brown
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <iomanip>
+#include <unordered_map>
+#define sz(s) (int(s.size()))
+#define MemS(s, x) memset(s, x, sizeof(s))
+#define PI acos(-1)
+typedef long long ll;
+using namespace std;
+
+void SADIEM()
+{
+    freopen("input.txt", "r", stdin);
+    //    freopen("output.txt", "w", stdout);
+
+    std::ios_base::sync_with_stdio(NULL);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+/*
+
+ “ I’d like to let you in on a very important secret I learned when I was about your age, boy.
+ You see, power, real power doesn’t come to those who were born strongest or fastest or smartest.
+ No. It comes to those who will do anything to achieve it.”
+
+*/
+void Catch_Kadr_fe_ALOLo()
+{
+    pair<int, int> p1, p2;
+    char op;
+    int ans = 0;
+
+    while (cin >> p1.first >> op >> p1.second >> op >> p2.first >> op >> p2.second)
+    {
+        if (p1 > p2)
+            swap(p1, p2);
+
+        ans += (p2.first >= p1.first && p2.first <= p1.second);
+        ans += (p2.second >= p1.first && p2.second <= p1.second);
+        ans -= (p1.first <= p2.first && p1.second >= p2.second);
+
+    }
+
+    cout << ans;
+}
+
+int main()
+{
+    SADIEM();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Catch_Kadr_fe_ALOLo();
+        cout << (t ? "\n" : "");
+    }
+}
