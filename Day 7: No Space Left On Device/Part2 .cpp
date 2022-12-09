f54fc7bb6134d651bb5495f866fdae7fe21a857f
Nayra Mohammed <97265976+NayraM0hammed@users.noreply.github.com>
2022-12-09 02:09:36 +0200

    Day 7 Done

--- Day 7: No Space Left On Device/Part2 .cpp
+++ Day 7: No Space Left On Device/Part2 .cpp
@@ -9,6 +9,27 @@
 #define PI acos(-1)
 typedef long long ll;
 using namespace std;
+const ll Mod = 1e9 + 7, INF = 1e18;
+const ll N = 1e5 + 1, O_O = LONG_LONG_MAX, T_T = INT_MAX, V_V = INT_MIN;
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, greater<int>, rb_tree_tag, tree_order_statistics_node_update> Set;
+typedef tree<pair<int, int>, null_type, greater_equal<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> Or_Set;
+*/
+// 8 neighbors
+
+int Dx[] = {-1, -1, -1, 0, 0, 1, 1, 1}; // Knight_mov:2, 1, -1, -2, -2, -1, 1, 2
+int Dy[] = {-1, 0, 1, -1, 1, -1, 0, 1}; // 1, 2, 2, 1, -1, -2, -2, -1
+
+// 4 neighbors
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
+
+string dir = "LRDU";
+//"Look deep into your soul, into the dark and foggy mist of your memories"
 
 void SADIEM()
 {
@@ -45,8 +66,7 @@ void Calc_Sz(vector<string> path)
 
 void Catch_Kadr_fe_ALOLo()
 {
-    ll ans = 0, tot = 0;
-
+    
     vector<string> path;
     string curr_dir = "/", s;
     path.push_back(curr_dir);
@@ -79,18 +99,10 @@ void Catch_Kadr_fe_ALOLo()
 
     for (auto it : Sz)
         ss.insert(it.second);
-    
-    tot = 70000000 - Sz[{"/"}];
-    
-    while (tot < 30000000)
-    {
-        auto it = ss.upper_bound(30000000 - tot);
-        if (it == ss.end())
-            it--;
-        tot += *it, ans += *it, ss.erase(it);
-    }
 
-    cout << ans;
+    auto it = ss.lower_bound(30000000 - (70000000 - Sz[{"/"}]));
+
+    cout << *it;
 }
 
 int main()
